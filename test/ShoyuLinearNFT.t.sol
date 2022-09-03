a2208265829b984992924e9041aaee8d8162dda0
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-09-03 15:46:39 +0900

    Create ShoyuLinearNFT.t.sol
    ✍️ test

---
+++ test/ShoyuLinearNFT.t.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import {DSTestPlus} from "solmate/test/utils/DSTestPlus.sol";
+
+import {LinearNFT} from "../src/ShoyuLinearNFT.sol";
+
+contract ShoyuLinearNFTTest is DSTestPlus {
+    ShoyuLinearNFT nft;
+
+    string internal name = "Example Linear NFT";
+
+    string internal symbol = "LINEAR";
+
+    string internal baseURI = "PLACEHOLDER";
+
+    int256 internal constant targetPrice = 69.42e18;
+
+    int256 internal constant priceDecayPercent = 0.31e18;
+
+    int256 internal constant perTimeUnit = 2e18;
+
+    function setUp() public {
+        nft = new ShoyuLinearNFT(
+            name,
+            symbol,
+            baseURI,
+            targetPrie,
+            priceDecayPercent,
+            perTimeUnit
+        );
+    }
+
+    function testMintNFT() public {
+        nft.mint{value: 83.571859212140979125e18}();
+
+        assertEq(nft.balanceOf(address(this)), 1);
+        assertEq(nft.ownerOf(0), address(this));
+    }
+
+    function testCannotUnderpayForNFTMint() public {
+        hevm.expectRevert("UNDERPAID");
+        nft.mint{value: 83e18}();
+    }
+
+    function testMintManyNFT() public {
+        for (uint256 i = 0; i < 100; i++) {
+            nft.mint{value: address(this).balance}();
+        }
+
+        assertEq(nft.balanceOf(address(this)), 100);
+        for (uint256 i = 0; i < 100; i++) {
+            assertEq(nft.ownerOf(i), address(this));
+        }
+    }
+
+    receive() external payable {}
+}
